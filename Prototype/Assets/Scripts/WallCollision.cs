0c98908248844461f291459bd00371fb1e9c4801
Benedict Chan <chanjhben@hotmail.com>
2017-05-04 18:34:39 +0800

    Movement issue

---
+++ Prototype/Assets/Scripts/WallCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "wall") {
+			Vector3 dir = col.contacts [0].point - this.transform.position;
+			dir.y = 0.0f;
+			this.transform.Translate (dir);
+//			this.GetComponent<Rigidbody> ().AddForce (dir * 20.0f);
+		}
+	}
+}
