038a98a1f4fb208cbf6ec5af2e7628bca4607294
Benedict Chan <chanjhben@hotmail.com>
2017-05-05 04:31:20 +0800

    Error with rotation

---
+++ Prototype/Assets/Scripts/PlayerControllerRigidbody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerRigidbody : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+		rb.rotation = Quaternion.Angle(transform.rotation, Quaternion.LookRotation (movement));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
