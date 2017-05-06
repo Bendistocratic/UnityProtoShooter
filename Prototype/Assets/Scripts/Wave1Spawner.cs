a4f7c5210ac76fa8538e73e403b9ce7261d23368
Benedict Chan <chanjhben@hotmail.com>
2017-05-06 22:04:38 +0800

    Added zombies, navmesh and spawnpoint.

---
+++ Prototype/Assets/Scripts/Wave1Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave1Spawner : MonoBehaviour {
+
+	public GameObject zombie;
+
+	private float counter;
+
+	// Use this for initialization
+	void Start () {
+		counter = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (counter <= 0.0f) {
+			Debug.Log (this.transform.position);
+			Instantiate (zombie, this.transform.position, Quaternion.identity);
+			counter = 5.0f;
+		} else {
+			counter -= Time.deltaTime;
+		}
+	}
+}
