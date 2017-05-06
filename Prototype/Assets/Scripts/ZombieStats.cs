fee6e4974b7ca4bb87c60d2c320bffa357c29a16
Benedict Chan <chanjhben@hotmail.com>
2017-05-07 05:17:22 +0800

    Added NavMesh

---
+++ Prototype/Assets/Scripts/ZombieStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieStats : MonoBehaviour {
+
+	private GameObject player;
+	private NavMeshAgent nma;
+	private Vector3 dest;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("character");
+		nma = this.GetComponent<NavMeshAgent> ();
+		dest = nma.destination;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (dest, player.transform.position) > 1.0f) {
+			nma.destination = player.transform.position;
+			dest = player.transform.position;
+		}
+	}
+}
