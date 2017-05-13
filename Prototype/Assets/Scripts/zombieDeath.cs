4f1256142d4dc6c740b1499c0b8fbe70ae66d66c
Benedict Chan <chanjhben@hotmail.com>
2017-05-13 17:52:45 +0800

    Updated with win / lose conditions. Added 3d sounds. (Part II)

---
+++ Prototype/Assets/Scripts/zombieDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieDeath : MonoBehaviour {
+
+	public AudioSource deathAudio;
+
+	private float destroyTimer = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+		deathAudio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (destroyTimer > 0.0f)
+			destroyTimer -= Time.deltaTime;
+		else 
+			Destroy(this.gameObject);
+	}
+}
